9c0479171f3d58ff79dd19d0bb9b4ea96ce5c553
rahmaidle <rahmaidle254@gmail.com>
2024-06-28 13:51:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+// index.js
+let dodger;
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left);
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  dodger = document.getElementById('dodger');
+});
